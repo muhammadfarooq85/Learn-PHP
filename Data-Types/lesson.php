58ce05279cdc751cbe1680c28fa0d0cdb1434385
Muhammad Farooq <irfankhans786787788@gmail.com>
2025-03-23 15:26:56 +0500

    data types

---
+++ Data-Types/lesson.php
@@ -0,0 +1,23 @@
+<?php
+
+// Scalar Types
+// boolean -> true or false
+// String -> single or ouble quotes enclosed characters
+// Int -> one  two three digits etc
+// Float -> 1.2, 1.3, 1.5 etc
+
+// how to check type of any data?
+// through var_dump() function
+// through gettype() function
+
+// Compound Types
+// Array -> []
+// print_r()
+// Object -> 
+// Iterble
+// Callable
+
+// Special Types
+// Resources
+// null
+?>
