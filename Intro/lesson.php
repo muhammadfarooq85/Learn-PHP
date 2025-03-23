41d9a83bd15654f05bd3a0cd0da32ce5e7ad1d81
Muhammad Farooq <irfankhans786787788@gmail.com>
2025-03-23 12:42:54 +0500

    intro to php

---
+++ Intro/lesson.php
@@ -0,0 +1,53 @@
+<!-- Php stands for hyper text preprocessor -->
+
+<!-- Open source and scripting language mainly used for web developemnt  -->
+
+<!-- java c++ requires compiler  -->
+
+<!-- but php reqires just interpreter -->
+
+<!-- php requires server to run -->
+
+<!-- if you are new to php and have not any language knowledge it's hard  -->
+
+<!-- if you are new to php and have any language specially JS knowledge it's easy to learn  -->
+
+<!-- Introduction to PHP
+PHP is a programming language that many devs use to create Web Applications, among other things.
+
+As a language, it had a humble beginning. It was first created in 1994 by Rasmus Lerdorf to build his personal website. He didn’t know at the time it would eventually become one of the most popular programming languages in the world. It became popular later on, in 1997/8, and exploded in the 2000s when PHP 4 landed.
+
+You can use PHP to add a little interactivity to an HTML page.
+
+Or you can use it as a Web Application engine that creates HTML pages dynamically and sends them to the browser.
+
+It can scale to millions of page views.
+
+Did you know Facebook is powered by PHP? Ever heard of Wikipedia? Slack? Etsy?
+
+What Kind of Language is PHP?
+Let’s get into some technical jargon.
+
+Programming languages are divided into groups depending on their characteristics. For example interpreted/compiled, strongly/loosely typed, dynamically/statically typed.
+
+PHP is often called a “scripting language” and it’s an interpreted language. If you’ve used compiled languages like C or Go or Swift, the main difference is that you don’t need to compile a PHP program before you run it.
+
+Those languages are compiled and the compiler generates an executable program that you then run. It’s a 2-steps process.
+
+The PHP interpreter is responsible for interpreting the instructions written in a PHP program when it’s executed. It’s just one step. You tell the interpreter to run the program. It's a completely different workflow.
+
+PHP is a dynamically typed language. The types of variables are checked at runtime, rather than before the code is executed as happens for statically typed languages. (These also happen to be compiled – the two characteristics often go hand in hand.)
+
+PHP is also loosely (weakly) typed. Compared to strongly typed languages like Swift, Go, C or Java, you don’t need to declare the types of your variables.
+
+Being interpreted and loosely/dynamically typed will make bugs harder to find before they happen at runtime.
+
+In compiled languages, you can often catch errors at compile time, something that does not happen in interpreted languages.
+
+But on the other hand, an interpreted language has more flexibility.
+
+Fun fact: PHP is written internally in C, a compiled and statically typed language.
+
+In its nature, PHP is similar to JavaScript, another dynamically typed, loosely typed, and interpreted language.
+
+PHP supports object-oriented programming, and also functional programming. You can use it as you prefer. -->
