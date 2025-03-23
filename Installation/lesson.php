7bb5fd09d55e894ebb154113a5b24f70cf803a3e
Muhammad Farooq <irfankhans786787788@gmail.com>
2025-03-23 12:54:31 +0500

    php intallation

---
+++ Installation/lesson.php
@@ -0,0 +1,7 @@
+<!-- Just install the XAAMP SERVER -->
+
+<!-- https://www.apachefriends.org/download.html -->
+
+<!-- Just install the php for you system -->
+
+<!-- https://www.php.net/downloads.php -->
