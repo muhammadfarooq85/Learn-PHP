933f689537ab09fe261cda7a0b4455dc60662f4c
Muhammad Farooq <irfankhans786787788@gmail.com>
2025-03-25 13:15:16 +0500

    Date & time

---
+++ Date/index.php
@@ -0,0 +1,12 @@
+<?php
+
+// Date & time
+
+// print out the current time in UNIX Timestamp format
+echo time();
+
+$todayDate = date("d-m-y");
+echo $todayDate;
+
+$currentTime = date("d-m-y,h:i:s");
+echo $currentTime;
