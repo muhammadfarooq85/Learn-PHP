6727ea570cb661e3191b96c308a665064bcd3c0a
Muhammad Farooq <irfankhans786787788@gmail.com>
2025-03-27 15:19:07 +0500

    OOP

---
+++ OOP/index.php
@@ -0,0 +1,61 @@
+<!-- OOP -->
+
+<!-- A programming language is generally classified based on its support for one or more paradigms. -->
+
+<!-- Object-oriented programming is one such paradigm, where the code is organized as objects. -->
+
+<!-- It is used to develop desktop and mobile applications or more complex web or enterprise applications.
+
+Using object-oriented programming, you can build modular and scalable software that is easy to maintain.
+
+In this article, you will learn about the principles of object-oriented Programming that lay the foundation for building robust systems.
+ -->
+
+<!-- What is Object-Oriented Programming? -->
+
+<!-- Object-Oriented Programming is a programming methodology that models real-world entities in the form of objects.
+
+These objects are instances of classes.
+
+A class can be thought of as a blueprint and each class can contain fields, which define the attributes of the object, and methods, which describe the object's behavior. Each class can be developed, tested, and debugged independently.
+
+Now that you have an understanding of the basic definition of object-oriented programming, let us jump right in and learn about its core principles.
+
+There are four core principles, or pillars, in the object-oriented Programming paradigm. They are:
+
+- Abstraction
+- Encapsulation
+- Inheritance
+- Polymorphism
+ -->
+
+ <!-- What is Abstraction? -->
+
+ <!-- Abstraction is defined as the concept of hiding the details of implementation and exposing only the necessary functionalities of the object to the user.
+
+The keywords that you need to keep in mind here are: 'Implementation hiding'. -->
+
+<!-- What is Encapsulation? -->
+
+<!-- Encapsulation refers to the concept of encapsulating or wrapping up the member variables (data) and the methods (behavior) of an object into a single unit, such as a class.
+
+The internal information of the object is hidden so that we can prevent unintended modifications and allow only controlled access. The keywords that you need to keep in mind here are: 'Information hiding'. -->
+
+<!-- What is Inheritance? -->
+
+<!-- Inheritance establishes a hierarchical relationship between two classes.
+
+In this concept, one class inherits the fields (properties) and methods (behavior) of another class, while having properties and behavior of its own.
+
+The class that inherits from another is called a subclass, derived class, or child class. The class from which the subclass inherits is known as the superclass, base class, or parent class. -->
+
+<!-- What is Polymorphism? -->
+
+<!-- Poly (many), morph (forms).
+
+Polymorphism defines the capability of an object to be represented in different forms. -->
+
+<!-- Conclusion -->
+<!-- In this article, we explored the fundamental principles of Object-Oriented Programming (OOP).
+
+Familiarity with these concepts is crucial for building robust, maintainable, and scalable software systems. -->
